03c677113ef8ce2dbadeb4365dc10db4703cd8bf
Aaron Hobson <aaron@ahob85.com>
2021-03-23 15:56:46 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,39 +1,3 @@
-/*
-
- // Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
-
-let classifier;
-
-// A variable to hold the image we want to classify.
-let img;
-
-function preload() {
-  classifier = ml5.imageClassifier("MobileNet");
-  img = loadImage("images/tiger.png");
-}
-
-function setup() {
-  createCanvas(400, 400);
-  classifier.classify(img, gotResult);
-  image(img, 0, 0);
-}
-
-// A function to run when we get any errors and the results
-let gotResult = (error, results) => {
-  // Display error in the console
-  if(error) {
-    console.error(error);
-  } else {
-    // The results are in an array ordered by confidence.
-    console.log(results);
-    createDiv(`Label: ${results[0].label}`);
-    if(results[0].label.includes("tiger")){
-      createDiv("It's a god damn tiger!");
-    }
-    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
-  }
-};
-*/
 let video;
 let poseNet;
 let poses = [];
@@ -95,3 +59,41 @@ function drawSkeleton() {
     }
   }
 }
+
+/*
+// Example of image recognition with MobileNet.
+
+ // Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+
+let classifier;
+
+// A variable to hold the image we want to classify.
+let img;
+
+function preload() {
+  classifier = ml5.imageClassifier("MobileNet");
+  img = loadImage("images/tiger.png");
+}
+
+function setup() {
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0);
+}
+
+// A function to run when we get any errors and the results
+let gotResult = (error, results) => {
+  // Display error in the console
+  if(error) {
+    console.error(error);
+  } else {
+    // The results are in an array ordered by confidence.
+    console.log(results);
+    createDiv(`Label: ${results[0].label}`);
+    if(results[0].label.includes("tiger")){
+      createDiv("It's a god damn tiger!");
+    }
+    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
+  }
+};
+*/
